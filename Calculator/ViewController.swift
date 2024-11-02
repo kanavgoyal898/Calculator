7c1743121c9f73a4fc6195a0b2511077435a4999
Kanav Goyal <kanavgoyal898@gmail.com>
2024-11-02 11:43:28 +0530

    Add Decimal Functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -3,29 +3,41 @@ import UIKit
 class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Invalid number: \(displayLabel.text!)")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        // collapse/expand: opt + cmd + <-/->
         //What should happen when a non-number button is pressed
         
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Invalid number: \(displayLabel.text!)")
-        }
-        
         if let calcMethod = sender.currentTitle {
             switch(calcMethod) {
-                case("AC"): displayLabel.text = "0"; break;
-                case("+/-"): displayLabel.text = String(-number); break;
-                case("%"): displayLabel.text = String(number / 100); break;
-                default: break;
+            case("AC"): displayValue = displayValue * 0; break;
+            case("+/-"): displayValue = displayValue * -1; break;
+            case("%"): displayValue = displayValue * 0.01; break;
+            default: break;
             }
         }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        // collapse/expand: opt + cmd + <-/->
         //What should happen when a number is entered into the keypad
         
         if let numValue = sender.currentTitle {
@@ -33,6 +45,12 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
